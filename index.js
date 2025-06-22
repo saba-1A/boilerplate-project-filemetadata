ec0ad42a9186c0f3f9f0985a5c869896f0335132
saba-1A <sabafathima789nazar@gmail.com>
2025-06-22 15:54:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
 var app = express();
 
 app.use(cors());
@@ -18,3 +21,10 @@ const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  console.log(req.file.originalname)
+  console.log(req.file.mimetype)
+  console.log(req.file.size)
+  res.send({name: req.file.originalname, type: req.file.mimetype, size: req.file.size})
+})
